d799e622a569c0b1a49c636737422cb79bb9f057
very-amused <very-amused@pm.me>
2023-12-03 00:56:38 -0500

    Rust day 1 part 1

--- rust/day1/src/p1.rs
+++ rust/day1/src/p1.rs
@@ -1,75 +1,68 @@
-use std::{io::{BufReader, BufRead}, fs::File, fmt::Display, error::Error, time::Instant};
+use std::{io::{BufReader, BufRead}, fs::File, error::Error, time::{Instant, Duration}};
 
 const INPUT_FILE: &str = "input.txt"; // Use sample.txt for testing
 
-// #region Parse error
-#[derive(Debug)]
-enum ParseError {
-  InvalidInput(String)
-}
-
-impl Display for ParseError {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    match self {
-      Self::InvalidInput(s) => write!(f, "{}", s)
-    }
-  }
-}
-
-impl Error for ParseError {}
-
-macro_rules! parse_err {
-  ($($arg:tt)*) => {
-    Err(ParseError::InvalidInput(format!($($arg)*)))
-  };
-}
-#[allow(unused_macros)]
-macro_rules! box_parse_err {
-  ($($arg:tt)*) => {
-    Err(Box::new(ParseError::InvalidInput(format!($($arg)*))))
-  };
-}
-
-// #endregion
-
 // #region Structs
-type Input = Vec<i32>; // Set input type, defaults to Vec<i32>
+struct Digits (i32, i32);
 
+impl Digits {
+	fn calibration_value(&self) -> i32 {
+		(10 * self.0) + self.1
+	}
+}
+
+type Input = Vec<Digits>;
 // #endregion
 
-// Parse 
+// Parse
 fn parse() -> Result<Input, Box<dyn Error>> {
   // Open scanner to read input line by line
   let file = File::open(INPUT_FILE)?;
   let reader = BufReader::new(file);
   
   // Parse lines
-  let mut input: Input = Vec::new();
+  let mut input = Vec::new();
   for l in reader.lines().map(|l| -> String {
     l.unwrap_or(String::new())
   }) {
     if l.len() == 0 {
+			continue;
     }
-    todo!();
+		// Find first/last single digit characters
+		let mut digits = Digits (-1, -1);
+		for c in l.split("") {
+			// Ignore non-digit characters
+			if let Ok(num) = c.parse() {
+				if digits.0 == -1 {
+					digits.0 = num;
+				}
+				digits.1 = num;
+			}
+		}
+		input.push(digits);
   }
+
   Ok(input)
 }
 
 // Solve
 fn solve(input: &mut Input) -> Result<String, Box<dyn Error>> {
-  todo!()
+	let mut sum = 0;
+	for pair in input {
+		sum += pair.calibration_value();
+	}
+	Ok(sum.to_string())
 }
 
-pub fn part1() {
+pub fn run() -> (String, Duration) {
   // Parse
   let mut input = parse().expect("Failed to parse input");
 
   // Solve
   let start = Instant::now();
   let solution = solve(&mut input).expect("Failed to solve");
-
-  // Report solve time and solution
   let duration = start.elapsed();
-  println!("Solved in \x1b[34m{:?}\x1b[0m", duration);
-  println!("Solution: \x1b[32m{}\x1b[0m", solution);
+
+  // Return solve time and solution
+	(solution, duration)
 }
