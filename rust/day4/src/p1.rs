d837d75cb918d83d4271259e73d59176a5d7f894
very-amused <very-amused@pm.me>
2023-12-09 23:02:18 -0500

    Create day 4 from template

---
+++ rust/day4/src/p1.rs
@@ -0,0 +1,43 @@
+use std::{io::{BufReader, BufRead}, fs::File, error::Error, time::{Instant, Duration}};
+
+const INPUT_FILE: &str = "input.txt"; // Use sample.txt for testing
+
+// #region Structs
+type Input = Vec<i32>; // Set input type, defaults to Vec<i32>
+
+// #endregion
+
+// Parse
+fn parse() -> Result<Input, Box<dyn Error>> {
+  // Open scanner to read input line by line
+  let file = File::open(INPUT_FILE)?;
+  let reader = BufReader::new(file);
+  
+  // Parse lines
+  let mut input: Input = Vec::new();
+  for l in reader.lines().filter_map(|l| l.ok()) {
+    if l.len() == 0 {
+			continue;
+    }
+    todo!();
+  }
+  Ok(input)
+}
+
+// Solve
+fn solve(input: &mut Input) -> Result<String, Box<dyn Error>> {
+  todo!()
+}
+
+pub fn run() -> (String, Duration) {
+  // Parse
+  let mut input = parse().expect("Failed to parse input");
+
+  // Solve
+  let start = Instant::now();
+  let solution = solve(&mut input).expect("Failed to solve");
+  let duration = start.elapsed();
+
+  // Return solve time and solution
+	(solution, duration)
+}
